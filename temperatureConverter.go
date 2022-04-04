cd7ad72d8ebceb31b9df06779d4b9a03e2d929b6
HunKiat <hunkiat@gmail.com>
2022-04-04 17:58:23 +0800

    okay

--- temperatureConverter.go
+++ temperatureConverter.go
@@ -2,22 +2,43 @@ package main
 
 import "fmt"
 
+func Celsius2Fahrenheit(c float64) float64 {
+	return (c*9/5 + 32)
+}
+
+func Celsius2Kelvin(c float64) float64 {
+	return (c + 273.15)
+}
+
+func Kelvin2Celsius(k float64) float64 {
+	return (k - 273.15)
+}
+
+func Fahrenheit2Celsius(f float64) float64 {
+	return ((f - 32) * 5 / 9)
+}
+
 func convertKelvin(temperatureInput float64) (float64, float64) {
-	//Insert your code here
+	// presumeing that the temperatureInput is Kelvin...
+	resultCelsius := Kelvin2Celsius(temperatureInput)
+	resultFahrenheit := Celsius2Fahrenheit(resultCelsius)
 
 	//Do not remove this line
 	return resultFahrenheit, resultCelsius
 }
 
 func convertCelsius(temperatureInput float64) (float64, float64) {
-	//Insert your code here
-
+	// presumeing that the temperatureInput is Celsius...
+	resultFahrenheit := Celsius2Fahrenheit(temperatureInput)
+	resultKelvin := Celsius2Kelvin(temperatureInput)
 	//Do not remove this line
 	return resultFahrenheit, resultKelvin
 }
 
 func convertFahrenheit(temperatureInput float64) (float64, float64) {
-	//Insert your code here
+	// presumeing that the temperatureInput is Fahrenheit...
+	resultCelsius := Fahrenheit2Celsius(temperatureInput)
+	resultKelvin := Celsius2Kelvin(resultCelsius)
 
 	//Do not remove this line
 	return resultKelvin, resultCelsius
@@ -33,17 +54,17 @@ func main() {
 
 	if temperatureChoice == 1 {
 		//Insert Code here
-
+		resultFahrenheit, resultCelsius := convertKelvin(temperatureInput)
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Celsius: ", resultCelsius)
 	} else if temperatureChoice == 2 {
 		//Insert Code here
-
+		resultFahrenheit, resultKelvin := convertCelsius(temperatureInput)
 		//DO not remove this
 		fmt.Println("Fahrenheit: ", resultFahrenheit, " Kelvin: ", resultKelvin)
 	} else if temperatureChoice == 3 {
 		//Insert Code here
-
+		resultCelsius, resultKelvin := convertFahrenheit(temperatureInput)
 		//DO not remove this
 		fmt.Println("Kelvin: ", resultKelvin, " Celsius: ", resultCelsius)
 	} else {
